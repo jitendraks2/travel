01f251323900d9ee78ed5f4dcc71ddb7bf8fa0cf
Jitendra <jitendraks1803@gmail.com>
2022-01-29 22:22:02 +0530

    changes

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
 function form() {
-    var name = document.forms["contactform"]["Name"];
-    var email = document.forms["contactform"]["Email"];
-    var phone = document.forms["contactform"]["mobilenumber"];
-    var drop = document.forms["contactform"]["Destination"];
-    
+  var name = document.forms["contactform"]["Name"];
+  var email = document.forms["contactform"]["Email"];
+  var phone = document.forms["contactform"]["mobilenumber"];
+  var drop = document.forms["contactform"]["Destination"];
 
-    if (name.value == "") {
-        window.alert("Please enter your name.");
-        name.focus();
-        return false;
-    }
+  if (name.value == "") {
+    window.alert("Please enter your name.");
+    name.focus();
+    return false;
+  }
 
-    if (email.value == "") {
-        window.alert(
-          "Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
+  if (email.value == "") {
+    window.alert("Please enter a valid e-mail address.");
+    email.focus();
+    return false;
+  }
 
-    if (phone.value == "") {
-        window.alert(
-          "Please enter your mobile number.");
-        phone.focus();
-        return false;
-    }
+  if (phone.value == "") {
+    window.alert("Please enter your mobile number.");
+    phone.focus();
+    return false;
+  }
 
+  if (drop.selectedIndex < 0) {
+    alert("Please enter your course.");
+    what.focus();
+    return false;
+  }
 
-    if (drop.selectedIndex < 0) {
-        alert("Please enter your course.");
-        what.focus();
-        return false;
-    }
+  return true;
+}
 
-    return true;
+let kashmir = document.getElementById("kashmir");
+let rajasthan = document.getElementById("rajasthan");
+let selectedOption;
+function destination() {
+  selectedOption = document.getElementById("selectDestination").value;
+
+  if (selectedOption == "Rajasthan") {
+    rajasthan.classList.remove("hidden");
+    kashmir.classList.add("hidden");
+
+  } 
+  
+  else if (selectedOption == "Kashmir") {
+    kashmir.classList.remove("hidden");
+    rajasthan.classList.add("hidden");
+
+  }
+
+  else{
+    rajasthan.classList.add("hidden");
+    kashmir.classList.add("hidden");
+
+  }
 }
